1453de2443ece2a7193cedbd894dfaec2fd5ee7f
SakubDev <sandomierzj@gmail.com>
2022-03-17 10:33:18 +0100

    Pretty code with prettier

--- src/components/DeleteEntryModal.js
+++ src/components/DeleteEntryModal.js
@@ -1,13 +1,22 @@
-import './DeleteEntryModal.css'
+import "./DeleteEntryModal.css";
 
-const DeleteEntryModal = ({closeModal}) => {
-    return <div className='modal__mask quickFadeIn'>
-        <div className="modal">
-            <p className="modal__heading">Are you sure you want to delete this entry?</p>
-            <button className={'modal__button button button-accent'} onClick={closeModal}>Cancel</button>
-            <button className={'modal__button button button-danger'}>Delete</button>
-        </div>
+const DeleteEntryModal = ({ closeModal }) => {
+  return (
+    <div className="modal__mask quickFadeIn">
+      <div className="modal">
+        <p className="modal__heading">
+          Are you sure you want to delete this entry?
+        </p>
+        <button
+          className={"modal__button button button-accent"}
+          onClick={closeModal}
+        >
+          Cancel
+        </button>
+        <button className={"modal__button button button-danger"}>Delete</button>
+      </div>
     </div>
-}
+  );
+};
 
-export default DeleteEntryModal
+export default DeleteEntryModal;
